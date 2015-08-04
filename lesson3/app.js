e18d8a70c58c7f245595a45dc6b74ea5a182d74c
kevinwang04 <kavinwang04@gmail.com>
2015-08-04 08:22:12 +0800

    to master from lab

--- lesson3/app.js
+++ lesson3/app.js
@@ -5,20 +5,24 @@ var superagent = require('superagent');
 var app = express();
 
 app.get('/', function (req, res, next) {
-  superagent.get('https://cnodejs.org/')
+  superagent.get('http://m.byr.cn/board/JobInfo')
     .end(function (err, sres) {
       if (err) {
         return next(err);
       }
       var $ = cheerio.load(sres.text);
       var items = [];
-      $('#topic_list .topic_title').each(function (idx, element) {
-        var $element = $(element);
+      var wrap = $('#m_main ul li a');
+      console.log(wrap[0]);
+      for (var i = 0; i < wrap.length; i++) {
+        // console.log(wrap[i]);
         items.push({
-          title: $element.attr('title'),
-          href: $element.attr('href')
-        });
-      });
+          
+          content:wrap[i].children[0].data,
+          href:"http://m.byr.cn/board/JobInfo"+wrap[i].attribs.href
+        })
+      };
+      
 
       res.send(items);
     });
